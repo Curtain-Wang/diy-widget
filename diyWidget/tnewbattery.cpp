74ff132069096b366cf8f4eca6f346f3688fde02
bblike <lzjgnh2002@gmail.com>
2024-07-16 15:00:41 +0800

    更新电源更改

--- diyWidget/tnewbattery.cpp
+++ diyWidget/tnewbattery.cpp
@@ -5,10 +5,32 @@
 #include <QLinearGradient>
 
 TNewBattery::TNewBattery(QWidget *parent)
-    : QWidget(parent), m_chargeLevel(50) { // 初始化电量为50%
+    : QWidget(parent), m_chargeLevel(70),m_batteryWidth(150), m_batteryHeight(80) { // 初始化电量为50%
     setMinimumSize(200, 140); // 增加最小宽度
 }
+int TNewBattery::batteryWidth() const {
+    return m_batteryWidth;
+}
 
+void TNewBattery::setBatteryWidth(int width) {
+    if (width != m_batteryWidth) {
+        m_batteryWidth = width;
+        emit batteryWidthChanged(width);
+        update();
+    }
+}
+
+int TNewBattery::batteryHeight() const {
+    return m_batteryHeight;
+}
+
+void TNewBattery::setBatteryHeight(int height) {
+    if (height != m_batteryHeight) {
+        m_batteryHeight = height;
+        emit batteryHeightChanged(height);
+        update();
+    }
+}
 int TNewBattery::chargeLevel() const {
     return m_chargeLevel;
 }
@@ -97,23 +119,22 @@ void TNewBattery::paintEvent(QPaintEvent *event) {
 
     // 设置字体大小
     QFont font = painter.font();
-    font.setPointSize(width()/20); // 增加字体大小，例如16
+    font.setPointSize(width() / 20);
     painter.setFont(font);
 
     // 电池主体
-    QRect batteryRect(width() * 2 / 10, height() * 2 / 10, width() * 6 / 10, height() * 6 / 10); // 0.6宽高，两边各留0.2的空间
+    QRect batteryRect(width() * 2 / 10, height() * 2 / 10, m_batteryWidth, m_batteryHeight);
 
     // 深蓝色长方形
     int borderSize = batteryRect.width() / 20;
     QRect blueRect(batteryRect.left() - borderSize, batteryRect.top() - borderSize, batteryRect.width() + borderSize * 2, batteryRect.height() + borderSize * 2);
+
     // 绘制电极
     drawElectrodes(painter, batteryRect);
     // 绘制电池主体和梯形
     drawBatteryBody(painter, batteryRect, blueRect);
     drawTrapezoid(painter, blueRect, borderSize, batteryRect.height() / 10);
     drawBase(painter, blueRect, borderSize, batteryRect.height() / 10);
-
-
 
     // 绘制充电百分比
     drawChargeLevel(painter, batteryRect);
--- diyWidget/tnewbattery.h
+++ diyWidget/tnewbattery.h
@@ -7,20 +7,33 @@ class TNewBattery : public QWidget
 {
     Q_OBJECT
 
+    Q_PROPERTY(int batteryWidth READ batteryWidth WRITE setBatteryWidth NOTIFY batteryWidthChanged)
+    Q_PROPERTY(int batteryHeight READ batteryHeight WRITE setBatteryHeight NOTIFY batteryHeightChanged)
+
 public:
     explicit TNewBattery(QWidget *parent = nullptr);
 
     int chargeLevel() const;
     void setChargeLevel(int level);
 
+    int batteryWidth() const;
+    void setBatteryWidth(int width);
+
+    int batteryHeight() const;
+    void setBatteryHeight(int height);
+
 signals:
     void chargeLevelChanged(int level);
+    void batteryWidthChanged(int width);
+    void batteryHeightChanged(int height);
 
 protected:
     void paintEvent(QPaintEvent *event) override;
 
 private:
     int m_chargeLevel;
+    int m_batteryWidth;
+    int m_batteryHeight;
 
     void drawBatteryBody(QPainter &painter, const QRect &batteryRect, const QRect &blueRect);
     void drawElectrodes(QPainter &painter, const QRect &batteryRect);
